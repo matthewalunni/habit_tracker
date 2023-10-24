a4b804063ca38e67c45b0fbae68ecdd0e95717cb
matthewalunni <matthew.alunni@gmail.com>
2023-10-23 22:43:50 -0400

    Log out functional

--- client/screens/Settings.tsx
+++ client/screens/Settings.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {borderRadius} from '../theme';
+import {auth} from '../firebase/firebase-config';
 
 const Settings = () => {
+  const logout = () => {
+    auth.signOut();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Settings!</Text>
+      <TouchableOpacity onPress={logout} style={styles.button}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,6 +24,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    marginVertical: 5,
+    borderRadius: borderRadius.lg,
+    padding: 15,
+    backgroundColor: 'tomato',
   },
 });
 
